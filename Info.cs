e05485eebb768e2a028d1051e3139bcc2b4f40cd
Luc <Gamezorf29087@hotmail.com>
2022-05-17 18:07:35 -0300

    Classe com informações básicas.

---
+++ Info.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace 
+{
+    public class Info
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Job { get; set; }
+
+        public Info() { }
+    }
+}
