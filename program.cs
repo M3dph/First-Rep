f9bda33d7124ccd94d94c68fa0b77552efa9556e
Luc <Gamezorf29087@hotmail.com>
2022-05-20 16:17:15 -0300

    +info.

--- program.cs
+++ program.cs
@@ -8,7 +8,7 @@ namespace Program
             System.Console.WriteLine("Comandos mostrados no curso: git config --global user.name \"NAME\"; git config --global user.email \"E-MAIL\"; git status.");
             System.Console.WriteLine("\tgit add; git commit; git restore.");
             System.Console.WriteLine("\tgit branch; git restore, git checkout \"nameBranch\"; git reset");
-            System.Console.WriteLine("\tgit checkout -b \"nameBranch\"");
+            System.Console.WriteLine("\tgit checkout -b \"nameBranch\"; git branch -d \"nameBranch\"");
         }
     }
 }
