2a29da5c6da3a4dcf53112c123ccf2d22c981607
Luc <Gamezorf29087@hotmail.com>
2022-05-20 20:55:08 -0300

    +info.

--- program.cs
+++ program.cs
@@ -10,7 +10,7 @@ namespace Program
             System.Console.WriteLine("\tgit branch; git restore, git checkout \"nameBranch\"; git reset");
             System.Console.WriteLine("\tgit checkout -b \"nameBranch\"; git branch -d \"nameBranch\"");
             System.Console.WriteLine("\tgit pull; git clone \"gitURL\"");
-            System.Console.WriteLine("\tgit stash; git stash list; git stash apply \"ID\"");
+            System.Console.WriteLine("\tgit stash; git stash list; git stash apply \"ID\", git stash show -p \"ID\"");
         }
     }
 }
