0a62b179ef95908e00284d468632a73552fad773
Luc <Gamezorf29087@hotmail.com>
2022-05-20 16:05:23 -0300

    Adicionando mais informações no programa principal.

--- program.cs
+++ program.cs
@@ -7,6 +7,7 @@ namespace Program
             System.Console.WriteLine("Estudando git...");
             System.Console.WriteLine("Comandos mostrados no curso: git config --global user.name \"NAME\"; git config --global user.email \"E-MAIL\"; git status.");
             System.Console.WriteLine("\tgit add; git commit; git restore.");
+            System.Console.WriteLine("\tgit branch; git restore, git checkout \"nameBranch\"; git reset");
         }
     }
 }
